1df9898b2ebda32033dbee5c322f121ce1848965
Alenchic <dyak_alena@mail.ru>
2017-12-14 02:30:24 +0300

    Ebaaa

---
+++ views/layouts/footer.php
@@ -0,0 +1,31 @@
+    <div class="page-buffer"></div>
+</div>
+
+<footer id="footer" class="page-footer"><!--Footer-->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="contactinfo">
+                                    <ul class="nav nav-pills">
+                                        <li><a href="#"><i class="fa fa-phone"></i> +7 455 000 11 22</a></li>
+                                        <li><a href="#"><i class="fa fa-envelope"></i> zefirka@gmail.com</a></li>
+                                    </ul>
+                                </div>
+            </div>
+        </div>
+    </div>
+</footer><!--/Footer-->
+
+
+
+<script src="/template/js/jquery.js"></script>
+<script src="/template/js/jquery.cycle2.min.js"></script>
+<script src="/template/js/jquery.cycle2.carousel.min.js"></script>
+<script src="/template/js/bootstrap.min.js"></script>
+<script src="/template/js/jquery.scrollUp.min.js"></script>
+<script src="/template/js/price-range.js"></script>
+<script src="/template/js/jquery.prettyPhoto.js"></script>
+<script src="/template/js/main.js"></script>
+
+</body>
+</html>
